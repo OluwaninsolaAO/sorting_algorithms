681525511ae8d9ae296bdadc7feb2b684e8be31e
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-19 19:46:46 +0100

    Task 00: Reverted

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,13 +12,12 @@ void bubble_sort(int *array, size_t size)
 	int temp, n_times = 0;
 	size_t i, j;
 	int h_v = array[0]; /* highest value */
-	int l_v = array[0]; /* lowest value */
 	size_t h_p = size; /* highest position */
-	size_t l_p = 0; /* lowest position */
+	bool sorted = true;
 
 	for (i = 0; i < h_p; i++)
 	{
-		for (j = l_p; j < h_p - 1; j++)
+		for (j = 0; j < h_p - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -26,17 +25,18 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
+
+				sorted = false;
 			}
 
-			h_v = h_v > array[j + 1] ? h_v : array[j + 1];
+			h_v = h_v >= array[j + 1] ? h_v : array[j + 1];
 			if (h_v == array[h_p - 1])
 				h_p--;
-			l_v = l_v < array[j + 1] ? l_v : array[j + 1];
-			if (l_v == array[l_p] && j != 0)
-				l_p++;
 
 			n_times++;
 		}
+		if (sorted)
+			break;
 	}
 	printf("======= ");
 	printf("%d total cycle.\n", n_times);
