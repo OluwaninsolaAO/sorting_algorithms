481a442204528cd3ee14b1a72a28f7009e202b3e
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-24 22:51:42 +0100

    Task 03: Work in progress

--- 3-main.c
+++ 3-main.c
@@ -10,7 +10,7 @@
 int main(void)
 {
     /*int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};*/
-    int array[] = {19, 2};
+    int array[] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
